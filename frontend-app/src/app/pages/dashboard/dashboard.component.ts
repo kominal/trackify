172d643e02c88346434cc1a26e61eb51fd855a02
Marc Schroeder <marc.schroeder@kominal.com>
2025-04-01 20:12:40 +0200

    Add basic dashboard

--- frontend-app/src/app/pages/dashboard/dashboard.component.ts
+++ frontend-app/src/app/pages/dashboard/dashboard.component.ts
@@ -1,15 +1,20 @@
 import { AsyncPipe, DatePipe, NgClass, NgStyle } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 import { TooltipModule } from 'primeng/tooltip';
 import { combineLatest, map } from 'rxjs';
+import { Project } from '../../api/backend-core/models/project';
 import { Record } from '../../api/backend-core/models/record';
 import { Task } from '../../api/backend-core/models/task';
 import { RecordHttpService } from '../../api/backend-core/services';
+import { groupBy } from '../../helpers/util';
 import { TaskService } from '../../services/task.service';
 import { TenantService } from '../../services/tenant.service';
 import { TrackingService } from '../../services/tracking.service';
 
 interface Section {
+  projectId?: string;
+  project?: Project;
   task: Task;
   record: Record;
   start: Date;
@@ -20,14 +25,24 @@ interface Section {
   active: boolean;
 }
 
+interface Summary {
+  project: Project;
+  duration: string;
+  totalBreak: string;
+  sections: Section[];
+  active: boolean;
+}
+
 interface Day {
   date: Date;
   sections: Section[];
+  expanded: boolean;
+  summaries: Summary[];
 }
 
 @Component({
   selector: 'app-dashboard',
-  imports: [AsyncPipe, NgClass, NgStyle, TooltipModule, DatePipe],
+  imports: [AsyncPipe, NgClass, NgStyle, TooltipModule, DatePipe, ButtonModule],
   templateUrl: './dashboard.component.html',
 })
 export class DashboardComponent {
@@ -64,32 +79,78 @@ export class DashboardComponent {
         const endOfDay = new Date(startOfDay);
         endOfDay.setHours(23, 59, 59, 999);
 
-        const day: Day = { date: new Date(startOfDay), sections: [] };
+        const day: Day = { date: new Date(startOfDay), sections: [], expanded: false, summaries: [] };
 
         const relevantRecords = displayableRecords.filter((record) => record.start < endOfDay && record.end > startOfDay);
 
         const sorted = relevantRecords.sort((a, b) => a.end.getTime() - b.end.getTime());
 
         for (const record of sorted) {
-          const start = record.start < startOfDay ? startOfDay : record.start;
-          const end = record.end > endOfDay ? endOfDay : record.end;
+          const start = new Date(record.start < startOfDay ? startOfDay : record.start);
+          const end = new Date(record.end > endOfDay ? endOfDay : record.end);
 
           const startMinute = start.getHours() * 60 + start.getMinutes() - 1;
           const endMinute = end.getHours() * 60 + end.getMinutes();
           const duration = `${Math.floor((endMinute - startMinute) / 60)
             .toString()
-            .padStart(2, '0')}:${((endMinute - startMinute) % 60).toString().padStart(2, '0')} h`;
+            .padStart(2, '0')}:${((endMinute - startMinute) % 60).toString().padStart(2, '0')}h`;
 
           const task = tasks.find((t) => t.task.uuid === record.taskId);
           if (task) {
-            day.sections.push({ record, task: task.task, start, end, startMinute, endMinute, duration, active: record.active || false });
+            day.sections.push({
+              record,
+              projectId: task.project?.uuid,
+              project: task.project,
+              task: task.task,
+              start,
+              end,
+              startMinute,
+              endMinute,
+              duration,
+              active: record.active || false,
+            });
           }
         }
+
+        day.summaries = Object.values(groupBy(day.sections, 'projectId')).map((sections): Summary => {
+          const project = sections[0].project as Project;
+          const duration = sections.reduce((acc, section) => {
+            const startMinute = section.start.getHours() * 60 + section.start.getMinutes() - 1;
+            const endMinute = section.end.getHours() * 60 + section.end.getMinutes();
+            console.log(section.start.getDate(), 'startMinute', startMinute, 'endMinute', endMinute, project.name, endMinute - startMinute);
+            return acc + (endMinute - startMinute);
+          }, 0);
+          const start = sections.reduce((acc, section) => (section.start < acc ? section.start : acc), sections[0].start);
+          const end = sections.reduce((acc, section) => (section.end > acc ? section.end : acc), sections[0].end);
+          const startMinute = start.getHours() * 60 + start.getMinutes() - 1;
+          const endMinute = end.getHours() * 60 + end.getMinutes();
+          const totalBreak = endMinute - startMinute - duration;
+
+          const durationString = `${Math.floor(duration / 60)
+            .toString()
+            .padStart(2, '0')}:${(duration % 60).toString().padStart(2, '0')}h`;
+          const totalBreakString = `${Math.floor(totalBreak / 60)
+            .toString()
+            .padStart(2, '0')}:${(totalBreak % 60).toString().padStart(2, '0')}h`;
+          return {
+            project,
+            duration: durationString,
+            totalBreak: totalBreakString,
+            sections,
+            active: sections.some((section) => section.active),
+          };
+        });
 
         days.push(day);
       }
 
-      return days.reverse();
+      days.reverse();
+
+      if (days.length > 0) {
+        days[0].expanded = true;
+      }
+
+      return days;
     }),
   );
 }
